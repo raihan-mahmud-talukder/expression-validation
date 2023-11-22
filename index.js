154fc5486d874aa081a117237563984e35b75343
Raihan Mahmud <raihan_uits@outlook.com>
2023-11-23 03:18:36 +0600

    reset button

--- index.js
+++ index.js
@@ -12,38 +12,53 @@ const section = document.querySelectorAll('section')
 
 for (let i = 0; i < section.length; i++) {
     section[i].addEventListener('click', () => {
+        document.getElementsByTagName('footer')[0].innerHTML = ''
         option = section[i].innerHTML
         switch (i) {
             case 0:
                 input = prompt()
-                if (/(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)/.test(input)) {
+                if (!input) {
+                    displayMessage(`Invalid input`)
+                } else if (/(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)/.test(input)) {
                     displayMessage(`Your ${option} '${input}' is Valid`)
                 } else displayMessage(`Your ${option} '${input}' is Invalid`)
                 break
             case 1:
                 input = prompt()
-                if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(input)) {
+                if (!input) {
+                    displayMessage(`Invalid input`)
+                } else if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(input)) {
                     displayMessage(`Your ${option} '${input}' is Valid`)
                 } else displayMessage(`Your ${option} '${input}' is Invalid`)
                 break
             case 2:
                 input = prompt('BD Mobile Number Only')
-                if (/^(\+88)?(88)?01[3-9]([0-9]){8}$/.test(input)) {
+                if (!input || isNaN(input)) {
+                    displayMessage(`Invalid input`)
+                } else if (/^(\+88)?(88)?01[3-9]([0-9]){8}$/.test(input)) {
                     displayMessage(`Your ${option} '${input}' is Valid`)
                 } else displayMessage(`Your ${option} '${input}' is Invalid`)
                 break
             case 3:
                 input = prompt('at least 1 uppercase, 1 lowercase, 1 number and minimum length 8')
-                if (/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$/g.test(input)) {
+                if (!input) {
+                    displayMessage(`Invalid input`)
+                } else if (/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$/g.test(input)) {
                     displayMessage(`Your ${option} '${input}' is Valid`)
                 } else displayMessage(`Your ${option} '${input}' is Invalid`)
                 break
             case 4:
                 input = prompt()
-                if (/^\d{4}$/.test(input)) {
+                if (!input) {
+                    displayMessage(`Invalid input`)
+                } else if (/^\d{4}$/.test(input)) {
                     displayMessage(`Your ${option} '${input}' is Valid`)
                 } else displayMessage(`Your ${option} '${input}' is Invalid`)
                 break
         }
     })
 }
+
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+    document.getElementsByTagName('footer')[0].innerHTML = ''
+})
